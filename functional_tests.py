a67fe7671a41c9551999a1721db5ffa474a92f90
Varveler <evarval@gmail.com>
2015-10-18 12:36:09 -0500

    Front page html now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -25,9 +25,9 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do', header_text)
 
 		# She is invited to enter a to-do item straight away
-		inputbox = self.browser.find_elemnt_by_id('id_new_item')
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
-			inputbox.get_atribute('placeholder'),
+			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 			)
 
@@ -38,10 +38,11 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_elemnt_by_id('id_list_table')
-		rows = table.find_elemnts_by_tag_name('tr')
-		slef.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows),
+			"New to do item did not appeat in table"
 			)
 
 		#There is still a text box inviting her to add another item. She
