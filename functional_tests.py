d4e4b5b252458c2b5f178fe6dba3888ca6180d20
Varveler <evarval@gmail.com>
2015-10-19 13:04:05 -0500

    changed function at the atomic level in the functional_test

--- functional_tests.py
+++ functional_tests.py
@@ -14,6 +14,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 
 		# Edith has heard about a cool new online to-do app. She goes
@@ -24,14 +29,14 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do lists', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
-		time.sleep(2)
+		
 		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 			)
-		time.sleep(2)
+		
 
 		#She types "Buy peacock feathers" into a text box
 		inputbox.send_keys('Buy peacock feathers')
@@ -39,17 +44,19 @@ class NewVisitorTest(unittest.TestCase):
 		#When she hits Enter, the page updates and now the page lists
 		# "1: Buy peacock feathers" as an item in a to-do list
 		inputbox.send_keys(Keys.ENTER)
-		time.sleep(2)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+
 		
 		#There is still a text box inviting her to add another item. She
 		# enters "Use peacock feathers to make a fly"
-		self.fail('Finish the test!')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(4)
+		# The page updates again, and now shows both items on her list
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-# The page updates again, and now shows both items on her list
 #Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some 
 # explanatory text to that effect
