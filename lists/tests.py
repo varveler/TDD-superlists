971ec318aba899ab9328083c9fc61ba6fbee453b
Varveler <evarval@gmail.com>
2015-10-20 07:30:35 -0500

    Model for lists Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
 
 
@@ -15,7 +16,7 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		
 		#resolve is the function Django uses interanlly to 
-		#resolve URLS and find what view funciont should map to
+		#resolve URLS and find what view function should map to
 		found = resolve('/')
 		#here we are checking in the found url '/' django finds a function called home_page
 		self.assertEqual(found.func, home_page)
@@ -37,7 +38,11 @@ class HomePageTest(TestCase):
 
 		# in order not to check constants we do the following code
 		expected_html = render_to_string('home.html')
+		# checks if the expected_html is the same as the request, decode() converts bytes into 
+		#strings
+		
 		self.assertEqual(response.content.decode(), expected_html)
+		print ("---> test home page return correct html")		
 
 	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
@@ -52,19 +57,25 @@ class HomePageTest(TestCase):
         {'new_item_text':  'A new list item'}
     )
 		self.assertEqual(response.content.decode(), expected_html)
+		print ('---> test home page can save a POST request')
 
 
 
+class ItemModelTest(TestCase):
 
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
 
+		second_item = Item()
+		second_item.text = "Item the second"
+		second_item.save()
 
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
 
-
-
-
-
-
-
-
-
-		
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, "Item the second")
