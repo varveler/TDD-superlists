d8d42a005046ff096a256f72e0530c893788bc7e
Varveler <evarval@gmail.com>
2015-10-24 19:50:43 -0500

    added new functional tests for two users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -44,9 +44,12 @@ class NewVisitorTest(LiveServerTestCase):
 		#She types "Buy peacock feathers" into a text box
 		inputbox.send_keys('Buy peacock feathers')
 
-		#When she hits Enter, the page updates and now the page lists
-		# "1: Buy peacock feathers" as an item in a to-do list
+		#When she hits Enter, shes taken to a new URL
+		#and now the page lists "1: Buy peacock feathers" as an item 
+		#in a to-do list
 		inputbox.send_keys(Keys.ENTER)
+		edith_list_url = self.browser.current_url
+		self.assertRegex(edith_list_url, '/list/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 		
@@ -61,6 +64,32 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 		self.fail('Finish the Tests!')
+
+		# Now a new user, Francis, comes along to the site.
+
+		## We use a new browser session to make sure that no information
+		## of edith is comming through from cookies etc
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		# Francis visits the home page. There is no  signg of Ediths
+		# list
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy peacock feathers', page_text)
+		slef.assertNotIn('make a fly', page_text)
+
+		#Francis starts a new list by entering a new item. He
+		# is less interesting than Edith
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Buy milk')
+		inputbox.send_keys(Keys.ENTER)
+
+		# Francis gets his own unique URL
+		francis_list_url = self.browser.current_url
+		self.assertRegex(francis_list_url, '/list/.+')
+		self.assertNotEqual(francis_list_url, edith_list_url)
+
 
 #Edith wonders whether the site will remember her list. Then she sees
 # that the site has generated a unique URL for her -- there is some 
