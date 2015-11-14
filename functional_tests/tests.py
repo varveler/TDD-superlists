dd61cde4024cb400b0ce418b9836b605314bcc7d
Varveler <evarval@gmail.com>
2015-11-14 09:53:16 -0600

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -6,6 +7,20 @@ import time
 
 #class NewVisitorTest(unittest.TestCase):
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.live_server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -26,7 +41,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check its homepage
 		#self.browser.get('http://localhost:8000')
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		# She notices the page title and heade mentio to-do lists
 		self.assertIn('To-Do lists', self.browser.title)
@@ -74,7 +89,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 		# Francis visits the home page. There is no  signg of Ediths
 		# list
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
@@ -93,7 +108,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#Edith goes to the home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024, 768)
 
 		#She notices the input box is nicely centered
