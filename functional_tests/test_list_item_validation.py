6809e8aa1d8e58f62036c7fbfd03441e100eff07
Varveler <evarval@gmail.com>
2015-12-21 19:49:36 -0600

    use get_absolute_url on list model to DRY urls in views

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,100 +9,33 @@ from .base import FunctionalTest
 class ItemValidationTest(FunctionalTest):
 
 
-	def test_can_start_a_list_and_retrieve_it_later(self):
-
-		# Edith has heard about a cool new online to-do app. She goes
-		# to check its homepage
-		#self.browser.get('http://localhost:8000')
-		self.browser.get(self.server_url)
-
-		# She notices the page title and heade mentio to-do lists
-		self.assertIn('To-Do lists', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
-		
-		# She is invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			inputbox.get_attribute('placeholder'),
-			'Enter a to-do item'
-			)
-		
-
-		#She types "Buy peacock feathers" into a text box
-		inputbox.send_keys('Buy peacock feathers')
-
-		#When she hits Enter, shes taken to a new URL
-		#and now the page lists "1: Buy peacock feathers" as an item 
-		#in a to-do list
-		inputbox.send_keys(Keys.ENTER)
-		edith_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, '/lists/.+')
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-		
-		#There is still a text box inviting her to add another item. She
-		# enters "Use peacock feathers to make a fly"
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
-		time.sleep(4)
-		# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-		
-
-		# Now a new user, Francis, comes along to the site.
-
-		## We use a new browser session to make sure that no information
-		## of edith is comming through from cookies etc
-		self.browser.quit()
-		self.browser = webdriver.Firefox()
-
-		# Francis visits the home page. There is no  signg of Ediths
-		# list
-		self.browser.get(self.server_url)
-		page_text = self.browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertNotIn('make a fly', page_text)
-
-		#Francis starts a new list by entering a new item. He
-		# is less interesting than Edith
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy milk')
-		inputbox.send_keys(Keys.ENTER)
-
-		# Francis gets his own unique URL
-		francis_list_url = self.browser.current_url
-		self.assertRegex(francis_list_url, '/lists/.+')
-		self.assertNotEqual(francis_list_url, edith_list_url)
-
-
-
-#Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some 
-# explanatory text to that effect
-
-# She visists that URL - her to-do list is still there
-
-#Satsified she goes back to sleep
-	@skip
 	def test_cannot_add_empty_list_items(self):
-		#Edith goes to home page and accidentaly tries to submit
-		# an empty list item. She hits Enter on the empty input inbox
+		# Edith goes to the home page and accidentally tries to submit
+		# an empty list item. She hits Enter on the empty input box
+		self.browser.get(self.server_url)
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-		#The home page refreshes and there is an error message saying
+		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
 
-		#She tries againwith some text for the item, wich now works
+		# She tries again with some text for the item, which now works
+		self.browser.find_element_by_id('id_new_item').send_keys('Buy milk\n')
+		self.check_for_row_in_list_table('1: Buy milk')
 
-		#Perversly, she now decides to submit a second blank list item.
+		# Perversely, she now decides to submit a second blank list item
+		self.browser.find_element_by_id('id_new_item').send_keys('\n')
 
-		#She recieves a similar warning on the list page
+		# She receives a similar warning on the list page
+		self.check_for_row_in_list_table('1: Buy milk')
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You can't have an empty list item")
 
-		#And she can correct it by filling some text in
-		self.fail('write me!')
+		# And she can correct it by filling some text in
+		self.browser.find_element_by_id('id_new_item').send_keys('Make tea\n')
+		self.check_for_row_in_list_table('1: Buy milk')
+		self.check_for_row_in_list_table('2: Make tea')
 
 
 #we removed this since now we are using python3 manage.py test functional_tests
--- lists/views.py
+++ lists/views.py
@@ -8,18 +8,7 @@ from lists.models import Item, List
 def home_page(request):
 	return render(request, 'home.html')
 
-def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	error = None
-	if request.method == 'POST':
-		try:
-			item = Item.objects.create(text=request.POST['item_text'], list=list_)
-			item.full_clean()
-			item.save()
-			return redirect('/lists/%d/' % list_.id)
-		except ValidationError:
-			error = "You can't have an empty list item"
-	return render(request, 'list.html', {'list':list_, 'error': error})
+
 
 def new_list(request):
 	list_ = List.objects.create()
@@ -31,4 +20,20 @@ def new_list(request):
 		list_.delete()
 		error = "You can't have an empty list item"
 		return render(request, 'home.html', {"error": error})
-	return redirect('/lists/%d/' % (list_.id,))
+	return redirect(list_)
+
+
+def view_list(request, list_id):
+	list_ = List.objects.get(id=list_id)
+	error = None
+
+	if request.method == 'POST':
+		try:
+
+			item = Item(text=request.POST['item_text'], list=list_)
+			item.full_clean()
+			item.save()
+			return redirect(list_)
+		except ValidationError:
+			error = "You can't have an empty list item"
+	return render(request, 'list.html', {'list':list_, 'error': error})
